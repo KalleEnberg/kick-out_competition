e4069fbf52f80138803c2220aed3ca6bde276734
Kalle Enberg <kalle.enberg@trustly.com>
2016-09-30 16:30:21 +0200

    troll

--- src/main/java/fourinarowbot/client/GameRunner.java
+++ src/main/java/fourinarowbot/client/GameRunner.java
@@ -6,8 +6,8 @@ import fourinarowbot.gameengine.HaxBot;
 public class GameRunner {
 
     public static void main(final String[] args) {
-        final String     playerName   = "MrHeyhey";
-        final String     gameName     = "MyGame1";
+        final String     playerName   = "L33TH4CK3R";
+        final String     gameName     = "EpicGame";
         final GameEngine myGameEngine = new HaxBot();
 
         RemoteGame.startGame(playerName, gameName, myGameEngine);
